70f7cfd0b83413e33c63aee60421698ea1610f62
Ramanuja <maringantiramanuja25@gmail.com>
2025-07-29 12:34:31 +0530

    admin

--- app/models.py
+++ app/models.py
@@ -1,8 +1,16 @@
 from django.db import models
-class student(models.Models):
+class Student(models.Model):
     name=models.CharField(max_length=100)
+
     rollno=models.IntegerField()
-def _str_(self):
-    return f"{self.name}-{self.rollno}"
+#def __str__(self):
+   # return f"{self.name}-{self.rollno}"
+class Details(models.Model):
+    phonenumber=models.IntegerField(max_length=10)
+    email=models.CharField(max_length=100)
+def __str__(self):
+    return f"{self.name}-{self.rollno}-{self.phonenumber}-{self.email}"
+
+
 
  # Create your models here.
